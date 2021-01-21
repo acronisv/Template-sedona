41b701ff8e398e0becdfbefa00fdaef8fe944170
acronisv <acronisv@gmail.com>
2021-01-21 03:13:05 +0300

    Update packages, gulpfile. Fix stylelint and editorconfig errors. Rename 'source' to 'src'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,94 @@
-"use strict";
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+const norm = [
+  './node_modules/normalize.css/normalize.css',
+];
 
-gulp.task("style", function() {
-  gulp.src("source/sass/style.scss")
-    .pipe(plumber())
+const sassFiles = [
+  'src/sass/*.scss'
+];
+
+const jsFiles = [
+  'src/js/*.js'
+];
+
+function styles(){
+  return gulp.src(norm)
+    .pipe(gulp.src(sassFiles))
     .pipe(sass())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
-});
+    .pipe(concat('template.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist:  ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({level: 2}))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+}
 
-gulp.task("serve", ["style"], function() {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
+function scripts(){
+  return gulp.src(jsFiles)
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
+}
 
-  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
-  gulp.watch("source/*.html").on("change", server.reload);
-});
+function html(){
+  return gulp.src('src/*.html')
+  .pipe(gulp.dest('./build'));
+}
+
+function fonts(){
+  return gulp.src('src/fonts/*.*')
+  .pipe(gulp.dest('./build/fonts'));
+}
+
+function watch(){
+  browserSync.init({
+        server: {
+            baseDir: "src/"
+    }
+    //tunnel: true
+    });
+  gulp.watch('src/sass/**/*.scss', styles);
+  gulp.watch('src/js/**/*.js', scripts);
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+}
+
+function image(){
+  return gulp.src('src/img/**/*.*')
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true
+    }))
+  .pipe(gulp.dest('./build/img'));
+}
+
+function clean(){
+  return del(['build/*'])
+}
+
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+gulp.task('watch', watch);
+gulp.task('clean', clean);
+gulp.task('image', image);
+gulp.task('html', html);
+gulp.task('fonts', fonts);
+
+/*финальная сборка проекта*/
+gulp.task('build', gulp.series('clean', 'html', 'fonts', 'styles', 'scripts', 'image'));
+
+/*сборка с очисткой папки build*/
+gulp.task('serve', gulp.series(clean, gulp.parallel(styles, scripts)));
+
+/*запуск проекта в browsersync*/
+gulp.task('dev', gulp.series('serve', 'watch'));
