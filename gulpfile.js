48987b200fbaa87361342248b791d30347e0a802
Алексей Гурецкий <algur@tut.by>
2021-08-23 14:52:17 +0300

    Возвращаем файлы проекта Седона (адаптив) в исходное состояние

--- gulpfile.js
+++ gulpfile.js
@@ -1,89 +1,51 @@
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
-const del = require('del');
-const imagemin = require('gulp-imagemin');
-const browserSync = require('browser-sync').create();
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sync = require("browser-sync").create();
 
-const sassFiles = [
-  'src/sass/*.scss'
-];
+// Styles
 
-const jsFiles = [
-  'src/js/*.js'
-];
-
-function styles(){
-  return gulp.src(sassFiles)
-    .pipe(sass())
-    .pipe(concat('template.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist:  ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(cleanCSS({level: 2}))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream());
+const styles = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("source/css"))
+    .pipe(sync.stream());
 }
 
-function scripts(){
-  return gulp.src(jsFiles)
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./build/js'))
-    .pipe(browserSync.stream());
+exports.styles = styles;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
 }
 
-function html(){
-  return gulp.src('src/*.html')
-  .pipe(gulp.dest('./build'));
+exports.server = server;
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/less/**/*.less", gulp.series("styles"));
+  gulp.watch("source/*.html").on("change", sync.reload);
 }
 
-function fonts(){
-  return gulp.src('src/fonts/*.*')
-  .pipe(gulp.dest('./build/fonts'));
-}
-
-function watch(){
-  browserSync.init({
-        server: {
-            baseDir: "src/"
-    }
-    //tunnel: true
-    });
-  gulp.watch('src/sass/**/*.scss', styles);
-  gulp.watch('src/js/**/*.js', scripts);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
-}
-
-function image(){
-  return gulp.src('src/img/**/*.*')
-    .pipe(imagemin({
-      optimizationLevel: 3,
-      progressive: true
-    }))
-  .pipe(gulp.dest('./build/img'));
-}
-
-function clean(){
-  return del(['build/*'])
-}
-
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
-gulp.task('watch', watch);
-gulp.task('clean', clean);
-gulp.task('image', image);
-gulp.task('html', html);
-gulp.task('fonts', fonts);
-
-/*финальная сборка проекта*/
-gulp.task('build', gulp.series('clean', 'html', 'fonts', 'styles', 'scripts', 'image'));
-
-/*сборка с очисткой папки build*/
-gulp.task('serve', gulp.series(clean, gulp.parallel(styles, scripts)));
-
-/*запуск проекта в browsersync*/
-gulp.task('dev', gulp.series('serve', 'watch'));
+exports.default = gulp.series(
+  styles, server, watcher
+);
